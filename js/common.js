24976507ab28f3ca7c189bc23d8a6a1584196a74
koinot-markazi <mahkamov4th@gmail.com>
2018-11-25 15:19:27 +0500

    fox

---
+++ js/common.js
@@ -0,0 +1,39 @@
+$(function() {
+
+	$('.start-project-a').mouseenter(function(){
+		$('.bg-circle').css({
+			width: "100px",
+			height: "100px"
+		});
+		$(this).css("color", "#fff");
+	}).mouseleave(function(){
+		$('.bg-circle').css({
+			width: "0px",
+			height: "0px"
+		})
+		$(this).css("color", "#FF522A");
+	})
+
+});
+
+// $(function(){
+// 	$('.index-container-right-projects-project-title').mouseenter(function(){
+// 		$(this).siblings('.index-container-right-projects-project-opacity').css({
+// 			opacity: "1"
+// 		});
+// 	}).mouseleave(function(){
+// 		$(this).siblings('.index-container-right-projects-project-opacity').css({
+// 			opacity: ".5"
+// 		});
+// 	})
+// })
+
+$('.projects-block').click(function(){
+	$(this).parent().siblings().slideUp(500);
+	$(this).siblings('#dscrpt').slideDown(500);
+});
+
+$('.projects-link-description-back').click(function(){
+	$('.projects-link').slideDown(500);
+	$(this).parents('#dscrpt').slideUp(500);
+})
